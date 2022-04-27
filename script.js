01489ab2f7896d54f501d10e1c8f948785db763e
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2022-04-27 16:43:02 +0200

    add flexbox on navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button")
+    if(!isDropdownButton && e.target.closest("[data-dropdown") != null) return
+    let currentDropDown
+    if(isDropdownButton){
+        currentDropDown = e.target.closest("[data-dropdown]")
+        currentDropDown.classList.toggle("active")
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown =>{
+        if (dropdown === currentDropDown) return
+        dropdown.classList.remove("active")
+    })
+})
