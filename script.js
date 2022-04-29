5a37a70b537251ef2e7238e3deda22d561296d4f
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2022-04-29 13:48:26 +0200

    Create eventlistener for features and compagny, complete the navbar mobile version

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ document.addEventListener("click", e => {
         dropdown.classList.remove("active")
     })
 })
+
+const features = document.querySelector('.features')
+const featureDrop = document.querySelector('.features-drop')
+
+features.addEventListener('click', function(){
+    featureDrop.classList.toggle('active')
+})
+
+const compagny = document.querySelector(".compagny")
+const compagnyDrop = document.querySelector('.compagny-drop')
+compagny.addEventListener('click', function(){
+    compagnyDrop.classList.toggle('active')
+})
