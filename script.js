5973e19311c44c9a566d5b679d21f0ac1f17c65f
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2022-04-29 15:01:15 +0200

    eventlistener for arrow nav and z index burger menu and main

--- script.js
+++ script.js
@@ -16,13 +16,18 @@ document.addEventListener("click", e => {
 
 const features = document.querySelector('.features')
 const featureDrop = document.querySelector('.features-drop')
-
+const iconFeature = document.querySelector('.icon-feature')
 features.addEventListener('click', function(){
     featureDrop.classList.toggle('active')
+    iconFeature.classList.toggle('active')
 })
 
 const compagny = document.querySelector(".compagny")
 const compagnyDrop = document.querySelector('.compagny-drop')
+const iconCompagny = document.querySelector('.icon-compagny')
+
+
 compagny.addEventListener('click', function(){
     compagnyDrop.classList.toggle('active')
+    iconCompagny.classList.toggle('active')
 })
